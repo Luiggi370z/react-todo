5f6890cf9fc6d35e3402eb81ca4387914aa23515
Luis Arpasi <meclipse@gmail.com>
2019-06-14 00:51:32 -0500

    Updates

---
+++ precache-manifest.46dd49784abdb8ff8ea88a973508b4c9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1929b4b3fedd82b95b0b2a0b18fdbd2",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "674bcd5af448f76311ab",
+    "url": "/react-todo/static/css/2.533df263.chunk.css"
+  },
+  {
+    "revision": "3a3c834c7673f982304a",
+    "url": "/react-todo/static/css/main.51c28d5b.chunk.css"
+  },
+  {
+    "revision": "674bcd5af448f76311ab",
+    "url": "/react-todo/static/js/2.0781a378.chunk.js"
+  },
+  {
+    "revision": "3a3c834c7673f982304a",
+    "url": "/react-todo/static/js/main.6d266e69.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "4d43beb267450852f6e7a849c54e16ab",
+    "url": "/react-todo/static/media/bubble-invert.4d43beb2.svg"
+  },
+  {
+    "revision": "10743dc8a7c90843ec164c96e1462c7f",
+    "url": "/react-todo/static/media/bubble.10743dc8.svg"
+  },
+  {
+    "revision": "3326c360137c2d6a6909d0c4303e502a",
+    "url": "/react-todo/static/media/icons-16.3326c360.eot"
+  },
+  {
+    "revision": "895bc7de59413bb8029ac83cac8c9e1b",
+    "url": "/react-todo/static/media/icons-16.895bc7de.woff"
+  },
+  {
+    "revision": "b6382c6d9ffca4a1c50f58a17fce15a9",
+    "url": "/react-todo/static/media/icons-16.b6382c6d.ttf"
+  },
+  {
+    "revision": "058e48cbbee6cdbfb2a2e6dab347f245",
+    "url": "/react-todo/static/media/icons-20.058e48cb.woff"
+  },
+  {
+    "revision": "aa8f3ca044cd237f87b69d0cb364887a",
+    "url": "/react-todo/static/media/icons-20.aa8f3ca0.ttf"
+  },
+  {
+    "revision": "e666d692a7dc07ea69886dfcec837c3c",
+    "url": "/react-todo/static/media/icons-20.e666d692.eot"
+  }
+]);
